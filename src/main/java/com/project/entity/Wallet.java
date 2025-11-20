a289cc3854aec056f382dff0b1fb7b4892f8303b
Fanantenana NyAina <hei.fanantenana@gmail.com>
2025-11-20 22:51:36 +0300

    fix: push attributes and getter

--- src/main/java/com/project/entity/Wallet.java
+++ src/main/java/com/project/entity/Wallet.java
@@ -1,4 +1,25 @@
 package com.project.entity;
 
 public class Wallet {
+    private String color;
+    private Double size;
+    private Double myBalance;
+
+    public Wallet(String color, Double size, Double myBalance) {
+        this.color = color;
+        this.size = size;
+        this.myBalance = myBalance;
+    }
+
+    public Double getSize() {
+        return size;
+    }
+
+    public Double getMyBalance() {
+        return myBalance;
+    }
+
+    public String getColor() {
+        return color;
+    }
 }
